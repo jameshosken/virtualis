c2c0a23cb9349f921e235bb5f80eb1abaaad4731
James Hosken <jameswfhosken@gmail.com>
2020-10-02 20:18:36 +0100

    Add full environment and baked lights

---
+++ Project/Virtualis/Assets/AlignToWallDirection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class AlignToWallDirection : MonoBehaviour
+{
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 100f))
+        {
+
+            Debug.DrawRay(transform.position, Vector3.down, Color.green);
+            Vector3 norm = hit.normal;
+
+            norm.Scale(new Vector3(1f, 0, 1f));
+
+            norm.Normalize();
+
+            transform.rotation = Quaternion.LookRotation(norm, Vector3.up);
+
+
+            Debug.DrawRay(transform.position, norm, Color.green);
+
+        }
+    }
+
+
+    
+}
---
+++ Project/Virtualis/Assets/FloatySignHover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatySignHover : MonoBehaviour
+{
+
+    [SerializeField] float speed = 0.1f;
+    [SerializeField] float amplitude = 1f;
+    Vector3 anchor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anchor = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = anchor + Vector3.up * Mathf.Sin(Time.time * speed) * amplitude;
+    }
+}
